bf065e52112fe405bca59c07369d31a83f22a79a
Manuel Raimann <raimannma@outlook.de>
2022-05-08 12:47:50 +0200

    Add Read The Docs Documentation

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+project = "Valorant API Wrapper"
+copyright = "2022, Valorant API Wrappr"
+author = "Manuel Raimann"
+
+version = "1.0.1"
+release = "1.0.1"
+
+language = None
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "autodocsumm",
+]
+
+add_module_names = False
+templates_path = ["templates"]
+source_suffix = ".rst"
+html_extra_path = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+pygments_style = "sphinx"
+todo_include_todos = True
+html_codeblock_linenos_style = "table"
+html_theme = "sphinx_rtd_theme"
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "undoc-members": True,
+    "private-members": True,
+}
---
+++ docs/content/index.rst
@@ -0,0 +1,12 @@
+Valorant API Wrapper
+====================
+
+.. automodule:: valo_api
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Python Packages
+   :glob:
+
+   _source_files/valo_api.endpoints
+   _source_files/valo_api.responses
