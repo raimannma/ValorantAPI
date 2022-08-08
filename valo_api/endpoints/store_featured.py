02b209f1b580e23b1db057d7611c39aab735bfec
NavisGames <83245135+NavisGames@users.noreply.github.com>
2022-08-09 01:26:11 +0200

    Update store_featured.py

--- valo_api/endpoints/store_featured.py
+++ valo_api/endpoints/store_featured.py
@@ -1,9 +1,10 @@
+from typing import Optional, Union
+
 from valo_api.endpoints_config import EndpointsConfig
 from valo_api.exceptions.valo_api_exception import ValoAPIException
 from valo_api.responses.error_response import ErrorResponse
-from valo_api.responses.store_featured import StoreFeaturedV1
+from valo_api.responses.store_featured import StoreFeaturedV1, StoreFeaturedV2, BundleV2
 from valo_api.utils.fetch_endpoint import fetch_endpoint
-
 
 def get_store_featured_v1(**kwargs) -> StoreFeaturedV1:
     """Get the store featured using version 1 of the endpoint.
@@ -19,18 +20,33 @@ def get_store_featured_v1(**kwargs) -> StoreFeaturedV1:
     """
     return get_store_featured("v1", **kwargs)
 
+def get_store_featured_v2(**kwargs) -> StoreFeaturedV2:
+    """Get the store featured using version 2 of the endpoint.
 
-def get_store_featured(version: str, **kwargs) -> StoreFeaturedV1:
+    This is the same as
+    :py:meth:`get_store_featured(version="v2", **kwargs) <get_store_featured>`
+
+    Args:
+        **kwargs: Any additional arguments to pass to the endpoint.
+
+    Returns:
+        StoreFeaturedV2: Store featured fetched from the API.
+    """
+    return get_store_featured("v2", **kwargs)
+
+def get_store_featured(version: str, **kwargs) -> Union[StoreFeaturedV1, StoreFeaturedV2]:
     """Get the store featured using a specific version of the endpoint.
 
     Args:
         version: The version of the endpoint to use.
             One of the following:
             v1 (Version 1)
+            v2 (Version 2)
         **kwargs: Any additional arguments to pass to the endpoint.
 
     Returns:
         StoreFeaturedV1: Store featured fetched from the API.
+        StoreFeaturedV2: Store featured fetched from the API.
 
     Raises:
         ValoAPIException: If the request failed.
@@ -47,5 +63,7 @@ def get_store_featured(version: str, **kwargs) -> StoreFeaturedV1:
         raise ValoAPIException(
             ErrorResponse.from_dict(headers=headers, **response_data)
         )
-
-    return StoreFeaturedV1.from_dict(**response_data["data"])
+    if version == "v1":
+        return StoreFeaturedV1.from_dict(**response_data["data"])
+    elif version == "v2":
+        return [BundleV2.from_dict(**Bundles) for Bundles in response_data["data"]]
