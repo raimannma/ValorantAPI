bf1d8c4c2a4a9f396f11325ba0ce44940d0edef7
Manuel Raimann <raimannma@outlook.de>
2022-09-03 16:27:10 +0200

    Add get account details by puuid endpoint

--- tests/e2e/test_account_details.py
+++ tests/e2e/test_account_details.py
@@ -13,16 +13,34 @@ from valo_api.exceptions.rate_limit import rate_limit
     id=st.sampled_from(["ManuelHexe#5777", "jasminaxrose#7024"]),
     force_update=st.booleans(),
 )
-def test_get_account_details(version: str, id: str, force_update: bool):
+def test_get_account_details_by_name(version: str, id: str, force_update: bool):
     sleep(rate_limit().reset + 1 if rate_limit().remaining <= 2 else 0)
     print(f"Test get_account_details with: {locals()}")
 
     name, tag = id.split("#")
 
-    getattr(valo_api, f"get_account_details_{version}")(
+    getattr(valo_api, f"get_account_details_by_name_{version}")(
         name=name, tag=tag, force_update=force_update
     )
 
 
+@settings(deadline=None, max_examples=15)
+@given(
+    version=st.sampled_from(["v1"]),
+    puuid=st.sampled_from(
+        ["ee89b4d9-13d0-5832-8dd7-eb5d8806d918", "930bb0d3-c914-5394-bcf6-0bc81f465bd7"]
+    ),
+    force_update=st.booleans(),
+)
+def test_get_account_details_by_puuid(version: str, puuid: str, force_update: bool):
+    sleep(rate_limit().reset + 1 if rate_limit().remaining <= 2 else 0)
+    print(f"Test get_account_details with: {locals()}")
+
+    getattr(valo_api, f"get_account_details_by_puuid_{version}")(
+        puuid=puuid, force_update=force_update
+    )
+
+
 if __name__ == "__main__":
-    test_get_account_details()
+    test_get_account_details_by_name()
+    test_get_account_details_by_puuid()
